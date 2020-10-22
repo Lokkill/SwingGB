bf4eed3ab3bfa05e361e02b5062bc4daf033d46a
Andrey Kataev <corvolk@yandex.ru>
2020-10-22 16:14:48 +0300

    Complete home work.

--- src/our/company/windows/Person.java
+++ src/our/company/windows/Person.java
@@ -4,5 +4,20 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
 
+    String secondName;
+    String firstName;
+    String middleName;
+    int age;
+    String phoneNumber;
+
+    public Person(String secondName, String firstName, String middleName, int age, String phoneNumber) {
+
+        this.secondName = secondName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
 
 }
--- src/our/company/windows/StartWindow.java
+++ src/our/company/windows/StartWindow.java
@@ -5,5 +5,66 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class StartWindow extends JFrame {
+    OperationOnData operationOnData = new OperationOnData();
+    ArrayList<Person> persons;
+    public StartWindow() {
+        persons = operationOnData.readInfo();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 600);
+        setTitle("First Swing window");
 
+        setLayout(new GridLayout(6, 2));
+
+        add(new JLabel("Фамилия"));
+        TextField secondNameTF = new TextField();
+        add(secondNameTF);
+
+        add(new JLabel("Имя"));
+        TextField firstNameTF = new TextField();
+        add(firstNameTF);
+
+        add(new JLabel("Отчество"));
+        TextField middleNameTF = new TextField();
+        add(middleNameTF);
+
+        add(new JLabel("Возраст"));
+        TextField ageTF = new TextField();
+        add(ageTF);
+        ageTF.addActionListener(e -> {
+            String text = operationOnData.checkIntType(ageTF.getText());
+            ageTF.setText(text);
+        });
+
+        add(new JLabel("Номер телефона"));
+        TextField phoneNumberTF = new TextField();
+        add(phoneNumberTF);
+
+        JButton saveBtn = new JButton("Сохранить");
+        add(saveBtn);
+        saveBtn.addActionListener(e -> {
+            Person person = new Person(secondNameTF.getText(),
+                    firstNameTF.getText(),
+                    middleNameTF.getText(),
+                    Integer.valueOf(ageTF.getText()),
+                    phoneNumberTF.getText());
+            persons.add(person);
+            operationOnData.saveInfo(persons);
+        });
+
+        JButton exitBtn = new JButton("Выход");
+        add(exitBtn);
+        exitBtn.addActionListener(e -> System.exit(-1));
+
+//        JMenuBar jbar = new JMenuBar();
+//        JMenu menu = new JMenu("Доп");
+//        JMenuItem table = new JMenuItem("Открыть таблицу");
+//        menu.add(table);
+//        table.addActionListener(e -> {
+//            GuideWindow gd = new GuideWindow();
+//        });
+//        jbar.add(menu);
+//        add(jbar);
+
+        setVisible(true);
+    }
 }
